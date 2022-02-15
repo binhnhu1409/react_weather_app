bae1d805a696986c84481b5a552bb2552fc9c1dc
binhnhu1409 <binhnhu1409@gmail.com>
2022-02-15 22:05:39 +0200

    create form for type in a location

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+
+  const [locationName, setLocationName] = useState()
+
+  const handleLocationChange = (event) => {
+    console.log('button clicked', event.target.value)
+    setLocationName(event.target.value)
+  }
+
+  return (
+    <form>
+      <input
+        value={locationName}
+        onChange={handleLocationChange}
+        placeholder='Please enter the name of a city'
+      />
+      <button type="submit">Search</button>
+    </form>
   )
 }
 
